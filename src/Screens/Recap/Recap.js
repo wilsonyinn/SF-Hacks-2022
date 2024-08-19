318c7cb74bb51b8f9f24a40e1903c7837c4601f1
Wilson Yin <wyin6610@gmail.com>
2024-08-19 16:45:53 -0700

    code clean up

--- src/Screens/Recap/Recap.js
+++ src/Screens/Recap/Recap.js
@@ -1,11 +1,8 @@
 import React from "react";
-// import ReactFullpage from "@fullpage/react-fullpage";
 import AwesomeSlider from "react-awesome-slider";
 import CoreStyles from "react-awesome-slider/src/core/styles.scss";
 import AnimationStyles from "react-awesome-slider/src/styled/fall-animation/fall-animation.scss";
 import recap1 from "../../Assets/Recap/1.png";
-// import recap2 from "../../Assets/Recap/2.png";
-// import recap3 from "../../Assets/Recap/3.png";
 import recap4 from "../../Assets/Recap/4.png";
 import recap5 from "../../Assets/Recap/5.png";
 import recap6 from "../../Assets/Recap/6.png";
@@ -19,7 +16,6 @@ import recap13 from "../../Assets/Recap/13.png";
 import recap14 from "../../Assets/Recap/14.png";
 import recap15 from "../../Assets/Recap/15.png";
 import recap16 from "../../Assets/Recap/16.png";
-import recap17 from "../../Assets/Recap/17.png";
 
 import "./Recap.css";
 
@@ -41,7 +37,6 @@ const Recap = () => (
         <div data-src={recap14} />
         <div data-src={recap15} />
         <div data-src={recap16} />
-        <div data-src={recap17} />
     </AwesomeSlider>
 );
 
